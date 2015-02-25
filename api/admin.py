37cb47b3103ff5d4d2d38dca7a78f9bf9dfd23be
Joseph <joseph@josephshearer.net>
2015-02-25 15:52:26 -0500

    Add admin models.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.site.register(Host)
+admin.site.register(Recording)
+admin.site.register(Transmitter)
+admin.site.register(Receiver)
+admin.site.register(Location)
+admin.site.register(WifiSettings)
+admin.site.register(CalculatedPosition)
