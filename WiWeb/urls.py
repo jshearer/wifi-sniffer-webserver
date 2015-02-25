e5bce87e6e7b5fde5a2270d43541f4a780af247d
Joseph <joseph@josephshearer.net>
2015-02-25 17:15:19 -0500

    Change urls around a bit

--- WiWeb/urls.py
+++ WiWeb/urls.py
@@ -18,7 +18,7 @@ router.register(r'calculatedpositions', views.CalculatedPositionViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
+    url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls))
 ]
